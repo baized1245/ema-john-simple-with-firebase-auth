4b483e387d384bf10e9b7c81840e383c793c86fe
unknown <you@example.com>
2023-07-29 00:13:48 +0600

    module-61.3

--- src/__steps.js
+++ src/__steps.js
@@ -7,4 +7,12 @@
 * 6.creat sign up and login route
 */ 
 
- 
+
+// context api
+/*
+* 1.creat a context provider file
+* 2.creat a context and set provider
+* 3.set the children props
+* 4.set context value
+* 5.set the provider
+*/  
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
@@ -16,6 +17,7 @@ const Login = () => {
                 </div>
                 <input className='btn-submit' type="submit" value="Login" />
             </form>
+            <p><small>New in ema-john? <Link to='/signup'>Creat new acount</Link> </small></p>
         </div>
     );
 };
